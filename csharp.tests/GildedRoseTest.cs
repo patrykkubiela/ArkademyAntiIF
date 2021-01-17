ccedd2ff08b6200de1c0783ab0856d92762f8e97
Patryk Kubiela <patryk.kubiela@programista-doswiadczony.pl>
2021-01-17 22:57:52 +0100

    add code coverage library

--- csharp.tests/GildedRoseTest.cs
+++ csharp.tests/GildedRoseTest.cs
@@ -15,6 +15,30 @@ namespace csharp.tests
             Assert.Equal("foo", Items[0].Name);
         }
 
+        [Theory]
+        [InlineData(22, 8, 20)]
+        [InlineData(23, 4, 20)]
+        [InlineData(0, 0, 20)]
+        public void BackstagePasses_Quality(int expected, int givenSellIn, int givenQuality)
+        {
+            var items = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = givenSellIn, Quality = givenQuality } };
+            var app = new GildedRose(items);
+            app.UpdateQuality();
+
+            Assert.Equal(expected, items[0].Quality);
+        }
+
+        [Theory]
+        [InlineData(22, 0, 20)]
+        public void AgedBrie_Quality(int expected, int givenSellIn, int givenQuality)
+        {
+            var items = new List<Item> { new Item { Name = "Aged Brie", SellIn = givenSellIn, Quality = givenQuality } };
+            var app = new GildedRose(items);
+            app.UpdateQuality();
+
+            Assert.Equal(expected, items[0].Quality);
+        }
+
         [Fact]
         public void TestReport()
         {
