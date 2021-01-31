5be8fba9871717dbe35ac8b51d0300c434ee28e3
Patryk Kubiela <patryk.kubiela@programista-doswiadczony.pl>
2021-01-31 18:04:46 +0100

    simplify good object build factory, change from if statement into switch

--- csharp/GoodCategory.cs
+++ csharp/GoodCategory.cs
@@ -2,6 +2,14 @@ namespace csharp
 {
     public class GoodCategory
     {
-        
+        public IGood BuildFor(Item item)
+        {
+            switch (item.Name)
+            {
+                case "Aged Brie": return new AgedBrie(item.Quality, item.SellIn);
+                case "Backstage passes to a TAFKAL80ETC concert": return new Backstage(item.Quality, item.SellIn);
+                default: return new Generic(item.Quality, item.SellIn);
+            }
+        }
     }
 }
