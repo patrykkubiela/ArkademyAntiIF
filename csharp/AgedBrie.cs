adfd84b115d823ae59f5c0a04c81041fba24f3f7
Patryk Kubiela <patryk.kubiela@programista-doswiadczony.pl>
2021-02-03 20:39:54 +0100

    create Expired version of AgedBrie goods

--- csharp/AgedBrie.cs
+++ csharp/AgedBrie.cs
@@ -16,5 +16,24 @@ namespace csharp
         {
             _quality.Increase();
         }
+        
+        public class Expired :IGood
+        {
+            private Quality _quality;
+
+            public int Quality => _quality.Amount;
+            public int SellIn { get; set; }
+
+            public Expired(int quality)
+            {
+                _quality = new Quality(quality);
+            }
+
+            public void Update(int? sellIn = null)
+            {
+                _quality.Increase();
+                _quality.Increase();
+            }
+        }
     }
 }
